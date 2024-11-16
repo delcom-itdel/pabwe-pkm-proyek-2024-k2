d308596670ed63b2ea55d1a98e066e7d51b0cbc3
Kevin Aditia <kevinaditiaarmandoharianja@gmail.com>
2024-11-16 09:20:47 +0700

    Update sarana.blade.php

--- resources/views/app/admin/sarana.blade.php
+++ resources/views/app/admin/sarana.blade.php
@@ -3,18 +3,12 @@
 @section('title', 'Sarana & Prasarana - SMAN 1 Balige')
 
 @section('content')
-<div class="container-fluid" data-aos="fade-up">
+<div class="content">
     <section id="prestasi" class="prestasi-section mt-5">
-        <div class="section-title">
-            <h2>Sarana & Prasarana</h2>
-        </div>
-
-        <!-- Table Section -->
-        <div class="card">
-            <div class="card-header">
-                Sarana & Prasarana
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Sarana & Prasarana</h2>
             </div>
-<<<<<<< Updated upstream
 
             <!-- Table Section -->
             <div class="card">
@@ -40,30 +34,8 @@
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addModal">
                             Tambah Data
                         </button>
-=======
-            @if (session()->has('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-            @endif
-            @if (session()->has('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-            @endif
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                    <div class="form-group">
-                        <label for="search">Search:</label>
-                        <input type="text" id="search" class="form-control" placeholder="Search">
->>>>>>> Stashed changes
                     </div>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addModal">
-                        Tambah Data
-                    </button>
-                </div>
 
-<<<<<<< Updated upstream
                     <!-- Modal Tambah -->
                     <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
@@ -95,49 +67,10 @@
                                         <button class="btn btn-primary" type="submit">Simpan</button>
                                     </div>
                                 </form>
-=======
-                <!-- Modal -->
-                <div class="modal fade" id="addModal" tabindex="-1" role="dialog"
-                    aria-labelledby="addModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="addModalLabel">Tambah Data</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
->>>>>>> Stashed changes
                             </div>
-                            <form action="{{ route('addsarana') }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label for="cover" class="form-label">Image</label>
-                                        <input type="file" class="form-control" id="cover" name="cover">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="nama" class="form-label">Nama</label>
-                                        <input type="text" class="form-control" id="nama" name="nama"
-                                            placeholder="Enter name">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="deskripsi" class="form-label">Deskripsi</label>
-                                        <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3"
-                                            placeholder="Enter description"></textarea>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-dismiss="modal">Close</button>
-                                    <button class="btn btn-primary" type="submit" id="submit"
-                                        name="submit">Simpan</button>
-                                </div>
-                            </form>
                         </div>
                     </div>
-                </div>
 
-<<<<<<< Updated upstream
                     <!-- Modal Edit -->
                     <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
@@ -194,48 +127,11 @@
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                         <button class="btn btn-danger" type="submit">Delete</button>
-=======
-                <!-- Modal -->
-                <div class="modal fade" id="editModal" tabindex="-1" role="dialog"
-                    aria-labelledby="editModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="editModalLabel">Edit Data</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                                    </div>
+                                </form>
                             </div>
-                            <form action="{{ route('editsarana') }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div class="modal-body">
-                                    <input type="hidden" name="sarana_id" value="">
-                                    <div class="mb-3">
-                                        <label for="cover" class="form-label">Cover(Optional)</label>
-                                        <input type="file" class="form-control" id="cover" name="cover">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="nama" class="form-label">Nama</label>
-                                        <input type="text" class="form-control" id="nama" name="nama"
-                                            placeholder="Enter name">
->>>>>>> Stashed changes
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="deskripsi" class="form-label">Deskripsi</label>
-                                        <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3"
-                                            placeholder="Enter description"></textarea>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-dismiss="modal">Close</button>
-                                    <button class="btn btn-primary" type="submit" id="submit"
-                                        name="submit">Simpan</button>
-                                </div>
-                            </form>
                         </div>
                     </div>
-<<<<<<< Updated upstream
 
                     <!-- Tabel Sarana & Prasarana -->
                     <table class="table table-striped">
@@ -264,74 +160,7 @@
                             @endforeach
                         </tbody>
                     </table>
-=======
->>>>>>> Stashed changes
                 </div>
-
-                <!-- Modal -->
-                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog"
-                    aria-labelledby="deleteModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Edit Data</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <form action="{{ route('deletesarana') }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <input type="hidden" name="sarana_id" value="">
-                                <div class="modal-body">
-                                    Apakah anda yakin menghapus Sarana dan Prasarana ini ?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-dismiss="modal">Close</button>
-                                    <button class="btn btn-danger" type="submit" id="submit"
-                                        name="submit">Delete</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-
-                <!-- Tabel Prestasi -->
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Image</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Deskripsi</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody id="prestasiTable">
-                        <?php $i = 1 ?>
-                        @foreach ($data['sarana'] as $sarana)
-                        <tr>
-                            <td>{{ $i++ }}</td>
-                            <td> <img src="{{ asset('sarana_img/' . $sarana['image']) }}" height="120rem"></td>
-                            <td>{{ $sarana['name'] }}</td>
-                            <td>{{ $sarana['description'] }}</td>
-                            <td>
-                                <!-- Tombol Edit -->
-                                <button type="button"
-                                    class="btn btn-warning btn-sm"
-                                    data-toggle="modal"
-                                    data-target="#editModal"
-                                    data-id="{{ $sarana['id'] }}">
-                                    Edit
-                                </button>
-                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-id="{{ $sarana['id'] }}" data-target="#deleteModal">Delete</button>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
             </div>
         </div>
     </section>
