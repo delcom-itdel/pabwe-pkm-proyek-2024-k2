75bc1e373db1be32faeaf31ef067ad91f9ea39a3
marthayulinda <marthayulindatobing@gmail.com>
2024-11-18 23:59:36 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row gy-4">
             <div class="col-lg-4 col-md-6 footer-about">
                 <a href="index.html" class="logo d-flex align-items-center">
-                    <span class="sitename">QuickStart</span>
+                    <span class="sitename">SMANSABAYOO</span>
                 </a>
                 <div class="footer-contact pt-3">
                     <p>SMA N 1 BALIGE</p>
